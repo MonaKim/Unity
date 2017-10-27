1a4887df706269709aca987af8877390a6949fbe
Mona Kim <hak045@naver.com>
2017-10-26 23:25:18 -0400

    change folder structure

--- 2017monajina-nothingfinalunityfiles/Funnel-master/FunnelPlugin/Funnel/Syphon/SyphonServerDrawingHelper.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <OpenGL/gltypes.h>
-
-@interface SyphonServerDrawingHelper : NSObject
-
-- (void)drawFrameTexture:(GLuint)texID surfaceSize:(NSSize)surfaceSize discardAlpha:(BOOL)discardAlpha;
-
-@end
