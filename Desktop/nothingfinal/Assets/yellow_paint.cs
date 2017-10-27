2fc0f51c795b3a515e7b9bcc0892b6c0037790c2
Mona Kim <hak045@naver.com>
2017-10-26 23:14:59 -0400

    first commit

---
+++ Desktop/nothingfinal/Assets/yellow_paint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yellow_paint: MonoBehaviour {
+	private GameObject target;
+	public GameObject brush;
+	public Canvas canvas;
+	float next_brush_time;
+	//Use this for initialization
+	void Start(){
+		target = GameObject.Find ("ARCamera");
+		next_brush_time = Time.time + 1f;
+	}
+
+	//Update is called once per frame
+	void Update(){
+		transform.LookAt (target.transform);
+
+		if (Input.GetKeyDown ("space")) {
+			canvas.enabled = false;
+		}
+
+		if (Input.GetKeyDown ("a")) {
+			canvas.enabled = true;
+		}
+		if (Input.GetMouseButton (0)) {		
+			Instantiate (canvas, target.transform.position, target.transform.rotation);
+			//next_brush_time+=if;
+		}
+		//Instantiate(target,transform.position,transform.rotation);
+	}
+}
---
+++ Desktop/nothingfinal/Assets/yellow_painting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yellow_painting : MonoBehaviour {
+	public GameObject yellow_paint;
+
+	//Update is called once per frame
+	void Update(){
+		
+		if(Input.GetMouseButton(0))
+		{
+				Instantiate(yellow_paint,transform.position,transform.rotation);
+
+	    }
+	}
+}
